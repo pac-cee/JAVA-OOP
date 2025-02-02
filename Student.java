5b2449b281cda2668e09045dcb7414f5904cc9a4
pacific <pacitekno12@gmail.com>
2025-02-02 17:32:26 +0200

    in class today

---
+++ Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main (String []arg){
+        String name = "pacifique bakundukize";
+        int id = 26798;
+        System.out.println("my id is" + id + " and my name is " + name);
+    }
+}
