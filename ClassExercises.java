3ab4b057b370363bf57df1bf73b7bef786265c46
pacific <pacitekno12@gmail.com>
2025-02-25 14:10:50 +0200

    refactor ClassExercises; modify Person class to remove static modifier, add Dog class with properties and methods

--- ClassExercises.java
+++ ClassExercises.java
@@ -1,15 +1,31 @@
 public class ClassExercises {
-    public static  class Person {
+    public class Person {
         private final String name;
         private final int age;
+        
         public Person(String name, int age){
             this.name = name;
             this.age = age;
         }
+        
+        public String getName(){ return name; }
+        public int getAge(){ return age; }
     }
-    public static void main(String[] args) {
-        Person person = new Person("Alice", 20);
-        System.out.println("Person created: " + person.name + ", Age: " + person.age);
 
-    }
+
+    public class Dog {
+        private String name;
+        private String breed;
+        
+        public Dog(String name, String breed){
+            this.name = name;
+            this.breed = breed;
+        }
+        
+        public String getName(){ return name; }
+        public String getBreed(){ return breed; }
+        
+        public void setName(String name){ this.name = name; }
+        public void setBreed(String breed){ this.breed = breed; }
+}
 }
